0baea21d15c120511407100170343dc00dbcde62
sanjana-soni2 <sanjana-soni2@ibm.com>
2024-08-13 17:57:42 +0530

    remove tf block

--- main.tf
+++ main.tf
@@ -6,17 +6,7 @@ data "template_file" "test" {
     sample_var = "${var.sample_var}"
   }
 }
-
-# This resource will destroy (potentially immediately) after null_resource.next
-resource "null_resource" "previous" {}
-
 resource "time_sleep" "wait_345600_seconds" {
-  depends_on = [null_resource.previous]
 
   create_duration = "345600s"
-}
-
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_345600_seconds]
 }
