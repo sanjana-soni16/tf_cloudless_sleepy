70a483bbd6020f5c7fdddd451b4cfe873bacccee
sanjana-soni2 <sanjana-soni2@ibm.com>
2024-08-13 17:40:29 +0530

    add sleep time

--- main.tf
+++ main.tf
@@ -7,12 +7,16 @@ data "template_file" "test" {
   }
 }
 
-resource "null_resource" "sleep" {
-  triggers = {
-    uuid = "${uuid()}"
-  }
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
 
-  provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
-  }
+resource "time_sleep" "wait_345600_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "345600s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_345600_seconds]
 }
--- vars.tf
+++ vars.tf
@@ -4,8 +4,3 @@ variable "sample_var" {
   description = "A sample_var to pass to the template."
   default     = "hello"
 }
-
-variable "sleepy_time" {
-  description = "How long our local-exec will take a nap."
-  default     = 5400
-}
